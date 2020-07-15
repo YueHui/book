d23b4ac0546c5cb7a3d208dd10687e06aaa3bb37
YueHui <527329923@qq.com>
2020-07-15 17:02:23 +0800

    添加箭头

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import * as PIXI from 'pixi.js';
 import Paper from './paper';
 import Flip from './flip';
+import Arrow from './arrow';
 
 const dragShadowImg = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGMAAAAyCAYAAABI6WXHAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAFLaVRYdFhNTDpjb20uYWRvYmUueG1wAAAAAAA8P3hwYWNrZXQgYmVnaW49Iu+7vyIgaWQ9Ilc1TTBNcENlaGlIenJlU3pOVGN6a2M5ZCI/Pgo8eDp4bXBtZXRhIHhtbG5zOng9ImFkb2JlOm5zOm1ldGEvIiB4OnhtcHRrPSJBZG9iZSBYTVAgQ29yZSA1LjYtYzE0MiA3OS4xNjA5MjQsIDIwMTcvMDcvMTMtMDE6MDY6MzkgICAgICAgICI+CiA8cmRmOlJERiB4bWxuczpyZGY9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiPgogIDxyZGY6RGVzY3JpcHRpb24gcmRmOmFib3V0PSIiLz4KIDwvcmRmOlJERj4KPC94OnhtcG1ldGE+Cjw/eHBhY2tldCBlbmQ9InIiPz6eHGDvAAABBUlEQVR4Xu3RywrCMBQG4dQ2vv/T9i54UnKkFNzPYj74TRWNixmO43i17fs+xq5z27ZxXdcpdp3Lsoyxdv42z3Nbzec4a579O9cZv6/9rhr3TnH/b/G/9TzPsZQyxertbHvfzufz87P8Ta7dk2v354bYP5++M3b07bGtb71t6Ztv5/M53z+Xd7Q78/7r/17xIghjgBgDxBggxgAxBogxQIwBYgwQY4AYA8QYIMYAMQaIMUCMAWIMEGOAGAPEGCDGADEGiDFAjAFiDBBjgBgDxBggxgAxBogxQIwBYgwQY4AYA8QYIMYAMQaIMUCMAWIMEGOAGAPEGCDGADEGiDFAjAFiDIxSvrXsgWjB4GEcAAAAAElFTkSuQmCC";
 
@@ -26,10 +27,6 @@ global = {
     lb: new PIXI.Point(0,config.paperHeight),
     currentCorner: 'rb'
 }
-
-window.global = global
-
-
 
 class Book {
     constructor(_config) {
@@ -147,7 +144,7 @@ class Book {
     }
     showBook() {
         this.showRight(this.currentIndex);
-
+        this.initArrows(); 
 
         this.app.stage.interactive = true;
         this.app.stage.on("mousedown", (e) => {
@@ -225,18 +222,70 @@ class Book {
         })
 
     }
+    initArrows(){
+        const leftArrow = new Arrow();
+        this.app.stage.addChild(leftArrow);
+        const rightArrow = new Arrow("right");
+        this.app.stage.addChild(rightArrow);
+
+        const leftX = global.middleTopPoint.x - config.paperWidth - 100;
+        leftArrow.x = Math.max(leftX,50);
+        const rightX = global.middleTopPoint.x + config.paperWidth + 100;
+        rightArrow.x = Math.min(rightX,config.width-50);
+        leftArrow.y = rightArrow.y = global.middleTopPoint.y + (config.paperHeight/2);
+        rightArrow.visible = false;
+
+        leftArrow.on("mousedown",e=>e.stopPropagation());
+        rightArrow.on("mousedown",e=>e.stopPropagation());
+
+        leftArrow.on("click",(e)=>{
+            e.stopPropagation();
+            
+            global.tween && global.tween.progress(1);
+
+            if(this.currentIndex>= config.images.length-1) return;
+            global.currentCorner = 'rb';
+            this.showRight(this.currentIndex+2)
+            this.pageContain.init(this.currentIndex,this.currentIndex+1);
+            this.pageContain.update(new PIXI.Point(global.rb.x-100, global.rb.y-100));
+
+            this.turnLeft(()=>{
+                rightArrow.visible = true;
+                if(this.currentIndex>= config.images.length-1){
+                    leftArrow.visible = false;
+                };
+            });
+            
+        });
+
+        rightArrow.on("click",(e)=>{
+            e.stopPropagation();
+            
+            global.tween && global.tween.progress(1);
+            
+            if(this.currentIndex <= 0 ) return;
+            global.currentCorner = 'lb';
+            this.showLeft(this.currentIndex-3);
+            this.pageContain.init(this.currentIndex-1,this.currentIndex-2);
+            this.pageContain.update(new PIXI.Point(global.lb.x+100, global.lb.y-100));
+            this.turnRight(()=>{
+                leftArrow.visible = true;
+                if(this.currentIndex === 2) rightArrow.visible = false;
+            });
+        })
+    }
     /**
      * 点击右面,向左翻页
      */
-    turnLeft(){
+    turnLeft(cb){
+        if(this.currentIndex>= config.images.length-1) return;
         this.pageContain.goNext(()=>{
             global.tween = null;
             this.showLeft(this.currentIndex+1);
             //如果后面还有页面
             this.currentIndex += 2;
             this.pageContain.removeChildren();
-
-            
+            cb && cb();
             if(!global.loader.resources[`img${config.images.length-1}`]){
                 this.loadMore();
             }
@@ -246,13 +295,15 @@ class Book {
     /**
      * 点击左边，向右翻页
      */
-    turnRight(){
+    turnRight(cb){
+        if(this.currentIndex <= 0 ) return;
         this.pageContain.goNext(()=>{
             global.tween = null;
             this.currentIndex -= 2;
             this.showLeft(this.currentIndex-1);
             this.showRight(this.currentIndex);
             this.pageContain.removeChildren();
+            cb && cb();
         });
     }
     /**
